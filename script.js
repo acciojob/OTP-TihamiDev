a4188fff7472b0d769fd3da2e89a0c62a7ad1b59
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-07-06 12:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+		console.log("Hello");
+		const codes = document.querySelectorAll(".code");
+		codes[0].focus();
+		
+		codes.forEach((input, index) => {
+			input.addEventListener("input", (e) => {
+				if(input.value.length === 1){
+					if(index<codes.length - 1){
+						codes[index+1].focus();	
+					}
+				}
+			})
+			
+			input.addEventListener("keyup", (e) => {
+				if(e.key === "Backspace" && input.value === "" && index>0){
+					codes[index-1].focus();
+				}
+			})
+		})
