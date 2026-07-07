49f1f8faf40414d74a42ab717ba11f8b690d1500
Tihami Rahman <138518399+TihamiDev@users.noreply.github.com>
2026-07-07 12:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 const codes = document.querySelectorAll(".code");
-		codes[0].focus();
-		console.log(codes.length);
-		codes.forEach((input, index) => {
-			input.addEventListener("input", (e) => {
-				if(!/^\d$/.test(input.value)){
-					input.value = "";
-					return;
-				}
-				
-				if(index<codes.length - 1){
-					codes[index+1].focus();	
-				}
-				
-			})
+	codes[0].focus();
+	// console.log(codes.length);
+	codes.forEach((input, index) => {
+		input.addEventListener("input", (e) => {
+			if(!/^\d$/.test(input.value)){
+				input.value = "";
+				return;
+			}
 			
-			input.addEventListener("keydown", (e) => {
-				if(e.key === "Backspace" && index>0){
-					// e.preventDefault();
-					codes[index-1].focus();
-				}
-			})
-		})	
-});
+			if(index<codes.length - 1){
+				codes[index+1].focus();	
+			}
+			
+		})
+		
+		input.addEventListener("keydown", (e) => {
+			if(e.key === "Backspace" && index>0){
+				// e.preventDefault();
+				codes[index-1].focus();
+			}
+		})
+	})	
+// });
 		
